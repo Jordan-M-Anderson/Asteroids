e46c626ec7ef7ce88c88c910440b069bc0a37d2f
Jordan-M-Anderson <47698298+Jordan-M-Anderson@users.noreply.github.com>
2020-10-14 11:36:00 -0500

    Empty Project

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+
+    private Vector2 mousePos;
+
+    [SerializeField]
+    public float moveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = Vector2.Lerp(transform.position, mousePos, moveSpeed);
+
+        }
+    }
+}
