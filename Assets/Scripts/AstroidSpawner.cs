f000d5c8326466787ea35be6606fbe95c80d2c4d
Jordan-M-Anderson <47698298+Jordan-M-Anderson@users.noreply.github.com>
2020-10-14 12:43:42 -0500

    Made Some Asteroids

---
+++ Assets/Scripts/AstroidSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AstroidSpawner : MonoBehaviour
+{
+    [SerializeField]
+    public int numOfAstroids;
+
+    public int currentNumOfAstroids;
+    private Vector2 pos;
+
+    [SerializeField]
+    public GameObject[] AsteroidPrefabs;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentNumOfAstroids != numOfAstroids && currentNumOfAstroids <= 4) {
+
+            spawnAsteroid();
+
+        }
+    }
+
+
+    private void spawnAsteroid() {
+
+        currentNumOfAstroids++;
+        pos = new Vector2(Random.Range(10, 11), Random.Range(-5, 4));
+        Instantiate(AsteroidPrefabs[Random.Range(0, AsteroidPrefabs.Length)], pos, Quaternion.identity);
+
+    }
+}
---
+++ Assets/Scripts/TeleporterX.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterX : MonoBehaviour
+{
+    public Transform teleportTarget;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var pos = other.transform.position;
+        pos.x = teleportTarget.position.x;
+        other.transform.position = pos;
+    }
+
+}
